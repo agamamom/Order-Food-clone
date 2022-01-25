7b4099f89586b12dfd5cc4312bfb2603988bd4bf
Dat <dathoang31082001@gmail.com>
2022-01-25 11:02:31 +0700

    first update

---
+++ components/Categories.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const item = [{
+    image: require("../assets/images/shopping-bag.png"),
+}]
+export default function Categories() {
+    return (
+        <View>
+            <Text>Categories</Text>
+        </View>
+    );
+}
